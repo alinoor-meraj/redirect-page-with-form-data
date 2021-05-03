5646e1502af831f7bab6de7453983f7fb66562bf
Ali-noor Meraj <alinoormeraj.fm@gmail.com>
2021-05-03 19:27:55 +0600

    Update Script.js

--- Script.js
+++ Script.js
@@ -8,8 +8,7 @@ const Province = pageUrlParam.get('province');
 const PostalCode = pageUrlParam.get('postalcode');
 const Email = pageUrlParam.get('email');
 const Phone = pageUrlParam.get('phone');
-const pageUrlWithParams = new 
-URL("http://REDIRECT_URL/?");
+const pageUrlWithParams = new URL("http://REDIRECT_URL/?");
 pageUrlWithParams.searchParams.append("fname", Fname);
 pageUrlWithParams.searchParams.append("lname", Lname);
 pageUrlWithParams.searchParams.append("address", Address);
@@ -20,9 +19,9 @@ pageUrlWithParams.searchParams.append("email", Email);
 pageUrlWithParams.searchParams.append("phone", Phone);
 jQuery(document).ready(function($) {
 	jQuery(".confirm_btn").click(function(){
-    	console.log("button clicked");
-    	console.log(pageUrlWithParams.href);
-	    window.location.href= pageUrlWithParams.href;
-    return false;
+		console.log("button clicked");
+		console.log(pageUrlWithParams.href);
+		window.location.href= pageUrlWithParams.href;
+		return false;
 	})
 });
